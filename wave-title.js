66466d2768229201abed8f02faf9b6ccd257119b
global-games <Jonas.schwinn99@gmail.com>
2025-11-22 12:50:29 +0100

    Fix script tag formatting in wave-title.js

--- wave-title.js
+++ wave-title.js
@@ -1,4 +1,3 @@
-<script>
 document.addEventListener("DOMContentLoaded", () => {
   const h1 = document.querySelector('h1');
   if (!h1) return;
@@ -11,4 +10,3 @@ document.addEventListener("DOMContentLoaded", () => {
     h1.appendChild(span);
   });
 });
-</script>
